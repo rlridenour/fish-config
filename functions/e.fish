2804de4e8be673fdf01ca21a25e75e7fba131ee2
Randy Ridenour <rlridenour@gmail.com>
2024-10-16 08:13:26 -0500

    Remove frame size from emacsclient function.

--- functions/e.fish
+++ functions/e.fish
@@ -1,5 +1,5 @@
 function e
-    emacsclient -c -F "((width  . (text-pixels . 940)) (height . (text-pixels . 1600)))" -n -a '' $argv
-		open -a Finder
-    open -a emacs
+    emacsclient -c -n -a '' $argv
+		# open -a Finder
+    # open -a emacs
 end
