072b93d9ba12b2233092aafe48c55610c2a2f545
Randy Ridenour <rlridenour@gmail.com>
2025-01-06 18:26:25 -0600

    Modify canvas function to allow coursetitle.

--- functions/canvas.fish
+++ functions/canvas.fish
@@ -1,7 +1,7 @@
 function canvas
     juice canvas.html canvas.html
     # find . -type f -name "canvas.html" -print0 | xargs -0 sed -i '' -e 's/body>/div>/g'
-    find . -type f -name "canvas.html" -print0 | xargs -0 sed -i '' -E '/body|title/d'
+    find . -type f -name "canvas.html" -print0 | xargs -0 sed -i '' -E '/body>|title>/d'
     pbcopy < "canvas.html"
     # rm canvas.html
 end
