47563c50d6a218f6dc73f1d255f3aae9f329cf40
Randy Ridenour <rlridenour@gmail.com>
2025-02-05 16:00:58 -0600

    Update notepad.

--- functions/notepad.fish
+++ functions/notepad.fish
@@ -1,5 +1,5 @@
 function notepad
     # emacsclient -c -F "((width . (text-pixels . 800)) (height . (text-pixels . 800)))" -n -a '' -e "(rlr-create-notepad-buffer)"
-    emacsclient -e "(rlr-create-notepad-buffer)"
+    emacsclient -e "(rlr/create-notepad-buffer)"
     open -a emacs
 end
