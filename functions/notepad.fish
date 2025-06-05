dc0a0a711558a259dd207be8f6a71a9fa1d6db3e
Randy Ridenour <rlridenour@gmail.com>
2025-06-05 14:21:18 -0500

    Updated.

--- functions/notepad.fish
+++ functions/notepad.fish
@@ -1,5 +1,6 @@
 function notepad
     # emacsclient -c -F "((width . (text-pixels . 800)) (height . (text-pixels . 800)))" -n -a '' -e "(rlr-create-notepad-buffer)"
+    # osascript ~/icloud/scripts/cut-clear.scpt
     emacsclient -e "(rlr/create-notepad-buffer)"
     open -a emacs
 end
--- functions/switch-paste.fish
+++
@@ -1,5 +0,0 @@
-function switch-paste
-    osascript -e "tell application \"System Events\" to keystroke tab using command down
-    # delay 1
-    # tell application \"System Events\" to keystroke (the clipboard as text)"
-end
