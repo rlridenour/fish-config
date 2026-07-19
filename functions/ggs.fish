0f8487f454e3abe7cfecadde14805e44fd45a0b0
Randy Ridenour <rlridenour@gmail.com>
2026-07-19 07:20:38 -0500

    Updated.

---
+++ functions/ggs.fish
@@ -0,0 +1,25 @@
+function ggs
+    printf \n
+    cd ~/github/ox-rlr-typst
+    printf 'ox-rlr-typst: %s' (fish_git_prompt)\n
+    cd ~/github/rlr-bib
+    printf 'rlr-bib: %s' (fish_git_prompt)\n
+    cd ~/github/rlr-create-typst-bib
+    printf 'rlr-create-typst-bib: %s' (fish_git_prompt)\n
+    cd ~/github/rlr-exam
+    printf 'rlr-exam: %s' (fish_git_prompt)\n
+    cd ~/github/teaching.el
+    printf 'teaching.el: %s' (fish_git_prompt)\n
+    cd ~/github/touying-basic-theme
+    printf 'touying-basic-theme: %s' (fish_git_prompt)\n
+    cd ~/github/touying-lecture
+    printf 'touying-lecture: %s' (fish_git_prompt)\n
+    cd ~/github/touying-slides-handout
+    printf 'touying-slides-handout: %s' (fish_git_prompt)\n
+    cd ~/github/typst-standard-form
+    printf 'typst-standard-form: %s' (fish_git_prompt)\n
+    cd ~
+end
+
+
+
